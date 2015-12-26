5457ff80e4733f7f5a1e427bde7ee8501f95c199
Steve Schaefer <stephen.schaefer@me.com>
2015-12-26 00:44:03 -0500

    Removed val list statement and modified formatting to shorten the code.

--- src/ChristmasFizzBuzz.scala
+++ src/ChristmasFizzBuzz.scala
@@ -3,5 +3,5 @@
   * Scala FizzBuzz Solution With A Christmas Twist
   */
 object ChristmasFizzBuzz extends App {
-  val list = List.range(1,101).foreach(i => println(if (i%15==0) "Merry Christmas" else if(i%5==0) "Christmas" else if(i%3==0) "Merry" else i))
+  List.range(1,101).foreach(i => println(if(i%15==0) "Merry Christmas" else if(i%5==0) "Christmas" else if(i%3==0) "Merry" else i))
 }
