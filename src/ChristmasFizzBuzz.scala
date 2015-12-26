d0286b524a6152ac7510751d9c59d3ad904ecca2
Steve Schaefer <stephen.schaefer@me.com>
2015-12-26 00:12:05 -0500

    Updated logic.

--- src/ChristmasFizzBuzz.scala
+++ src/ChristmasFizzBuzz.scala
@@ -3,5 +3,5 @@
   * Scala FizzBuzz Solution With A Christmas Twist
   */
 object ChristmasFizzBuzz extends App {
-  val list = List.range(1,101).foreach(i => println(if ((i%5==0)&&(i%3==0)) "Merry Christmas" else if((i%5==0)&&(i%3!=0)) "Christmas" else if((i%5!=0)&&(i%3==0)) "Merry" else i))
+  val list = List.range(1,101).foreach(i => println(if (i%15==0) "Merry Christmas" else if(i%5==0) "Christmas" else if(i%3==0) "Merry" else i))
 }
