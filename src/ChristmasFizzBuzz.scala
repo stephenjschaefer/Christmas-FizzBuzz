f8193d032e7418aab2988fe0b4867a7ea3514017
Steve Schaefer <stephen.schaefer@me.com>
2015-12-29 23:53:49 -0500

    Switched to for i <- 1 to 100 instead of list. Modified logic to use a tuple2, searching for a match and then printing the second element of the match or i if no match is found.

--- src/ChristmasFizzBuzz.scala
+++ src/ChristmasFizzBuzz.scala
@@ -3,5 +3,5 @@
   * Scala FizzBuzz Solution With A Christmas Twist
   */
 object ChristmasFizzBuzz extends App {
-  List.range(1,101).foreach(i => println(if(i%15==0) "Merry Christmas" else if(i%5==0) "Christmas" else if(i%3==0) "Merry" else i))
+  for (i <- 1 to 100) println(Seq(15->"Merry Christmas",5->"Christmas",3->"Merry").find(i%_._1==0).map(_._2).getOrElse(i))
 }
